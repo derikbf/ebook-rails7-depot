2568a89ec153f5c61666de5ed453e56454a8922a
derikbf <derikb@gmail.com>
2022-10-22 14:44:06 -0300

    Updated Cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,25 +1,21 @@
 class CartsController < ApplicationController
-  before_action :set_cart, only: %i[ show edit update destroy ]
+  before_action :set_cart, only: %i[show edit update destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
-  # GET /carts or /carts.json
   def index
     @carts = Cart.all
   end
 
-  # GET /carts/1 or /carts/1.json
   def show
   end
 
-  # GET /carts/new
   def new
     @cart = Cart.new
   end
 
-  # GET /carts/1/edit
   def edit
   end
 
-  # POST /carts or /carts.json
   def create
     @cart = Cart.new(cart_params)
 
@@ -34,7 +30,6 @@ class CartsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /carts/1 or /carts/1.json
   def update
     respond_to do |format|
       if @cart.update(cart_params)
@@ -47,24 +42,28 @@ class CartsController < ApplicationController
     end
   end
 
-  # DELETE /carts/1 or /carts/1.json
   def destroy
-    @cart.destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: "Cart was successfully destroyed." }
+      format.html { redirect_to store_index_path, notice: "Your cart is currently empty." }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_cart
-      @cart = Cart.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def cart_params
-      params.fetch(:cart, {})
-    end
+  def set_cart
+    @cart = Cart.find(params[:id])
+  end
+
+  def cart_params
+    params.fetch(:cart, {})
+  end
+
+  def invalid_cart
+    logger.error "Attempt to access invalid cart #{params[:id]}"
+    redirect_to store_index_url, notice: 'Invalid cart'
+  end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -65,14 +65,6 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
       delete product_url(products(:two))
     end
 
-    assert_redirect_to products_url
-  end
-
-  test "should destroy product" do
-    assert_difference("Product.count", -1) do
-      delete product_url(@product)
-    end
-
     assert_redirected_to products_url
   end
 end
